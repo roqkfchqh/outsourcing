03a94176c4953cadc5125e47741b45020ad4ed49
yeongbinim <yeim.dev@gmail.com>
2025-01-09 14:59:37 +0900

    feat(search): Like문으로 통합 검색기능 구현
    # Conflicts:
    #	src/main/java/com/example/outsourcing/domain/shop/entity/Menu.java
    #	src/main/java/com/example/outsourcing/domain/shop/repository/MenuRepository.java
    #	src/main/java/com/example/outsourcing/domain/shop/repository/MenuRepositoryCustom.java
    #	src/main/java/com/example/outsourcing/domain/shop/repository/MenuRepositoryImpl.java
    #	src/main/java/com/example/outsourcing/domain/shop/repository/ShopRepository.java
    #	src/main/java/com/example/outsourcing/domain/shop/repository/ShopRepositoryCustom.java
    #	src/main/java/com/example/outsourcing/domain/shop/repository/ShopRepositoryImpl.java

--- src/main/java/com/example/outsourcing/domain/shop/repository/ShopRepository.java
+++ src/main/java/com/example/outsourcing/domain/shop/repository/ShopRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ShopRepository extends JpaRepository<Shop, Long> {
+public interface ShopRepository extends JpaRepository<Shop, Long>, ShopRepositoryCustom {
 
     // 특정 사용자가 삭제되지 않은 가게를 보유하고 있는지 확인
     boolean existsByUserIdAndIsDeletedFalse(Long userId);
-    
+
 }
